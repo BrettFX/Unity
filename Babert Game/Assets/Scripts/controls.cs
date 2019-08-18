0ee19c2917d2f0a701d8a05c1424d45582dba2e1
Brett Allen <brettallen777@gmail.com>
2019-08-18 00:24:03 -0400

    Complete tutorial 3

---
+++ Babert Game/Assets/Scripts/controls.cs
@@ -0,0 +1,53 @@
+﻿// Jimmy Vegas Unity Tutorial
+// This Script will allow you to control the rocket
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls : MonoBehaviour
+{
+    public float speed = 8.0f;
+    public float jumpspeed = 20.5f;
+    public float gravity = 20.0f;
+    public Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Horizontal") + 3);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y += jumpspeed;
+            }
+
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y += jumpspeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
