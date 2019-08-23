70be57e175117b3f9839318ac49e0d952dc334b3
Brett Allen <brettallen777@gmail.com>
2019-08-23 18:24:55 -0400

    Complete tutorial 6

---
+++ Babert Game/Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    private const float INCREASE_RATE = 0.1f; 
+    private const int START_DELAY = 0; // In seconds
+    private const int SCORE_DELTA = 5; // Set to zero if only collectables should increase score
+
+    public static int score;
+    public GameObject scoreDisplay;
+
+    void Start()
+    {
+        InvokeRepeating("AddScore", START_DELAY, INCREASE_RATE);
+    }
+
+
+    void AddScore()
+    {
+        score += SCORE_DELTA;
+        scoreDisplay.GetComponent<Text>().text = "Score: " + score;
+    }
+}
