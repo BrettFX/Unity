0a70ce7691654efd4a63ed1eed4fb82834eced90
Brett Allen <brettallen777@gmail.com>
2019-08-24 00:42:32 -0400

    Continue working on tutorial 5 Left off at https://youtu.be/xUJFwW-9tTk?list=PLZ1b66Z1KFKiLGbRHcVltFitp0gL1inbo&t=795

---
+++ Babert Game/Assets/Scripts/StarPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StarPickup : MonoBehaviour
+{
+    // Jimmy Vegas Unity Tutorial
+    // This Script will allow you to collect your star
+
+    public int starScore = 500;
+    public AudioSource starSound;
+
+    void OnTriggerEnter(Collider col)
+    {
+        starSound.Play();
+        ScoringSystem.score += starScore;
+
+        // Place the object (transform) that is attached off the screen (1000 units on the y-axis)
+        this.transform.position = new Vector3(0, 1000, 0);
+    }
+}
