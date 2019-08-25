374cf9999729f6327b1a7d4f5400479219c358d8
Brett Allen <brettallen777@gmail.com>
2019-08-25 19:40:59 -0400

    Complete tutorial 8 Tutorial 9 link: https://www.youtube.com/watch?v=RVOEoDTqOZw&list=PLZ1b66Z1KFKiLGbRHcVltFitp0gL1inbo&index=9

---
+++ Babert Game/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    // Jimmy Vegas Unity Tutorial
+    // This script will activate your death screen and deactivate the rocket
+
+    public GameObject gameOverScreen;
+    public GameObject gameOverText;
+    public GameObject rocket;
+
+    void OnTriggerEnter(Collider col)
+    {
+        gameOverScreen.SetActive(true);
+        gameOverText.SetActive(true);
+        rocket.SetActive(false);
+    }
+}
