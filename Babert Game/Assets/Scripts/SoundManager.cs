22a1382309148f7a5693f99aea41a0e96b8111d1
Brett Allen <brettallen777@gmail.com>
2019-09-15 15:47:59 -0400

    Work on implementing in-game volume toggle capability Plan is to toggle volume and sound effects when the volume button is clicked

---
+++ Babert Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    private bool m_volumeToggle = true;
+
+    public Button btnVolume;
+    public Sprite spriteVolOn;
+    public Sprite spriteVolOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO get from player prefs
+        m_volumeToggle = true;
+    }
+
+    // Toggle the volume state
+    public void ToggleVolumeState()
+    {
+        m_volumeToggle = !m_volumeToggle;
+        Sprite targetSprite = m_volumeToggle ? spriteVolOn : spriteVolOff;
+        btnVolume.GetComponent<Image>().sprite = targetSprite;
+    }
+}
