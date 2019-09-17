be4d4423d35bafd4731447d213c0e845195bc785
Brett Allen <brettallen777@gmail.com>
2019-09-17 19:27:31 -0400

    Create settings manager script This script will keep track of all the associated settings slider values in a make-shift dictionary structure (list) Leverages player prefs that can be referenced for the slider values and apply settings in-game accross other scenes

---
+++ Babert Game/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public enum Setting
+    {
+        MASTER_VOLUME,
+        MUSIC,
+        SFX
+    }
+
+    // Private player prefs keys
+    private const string MASTER_VOLUME_KEY = "MasterVolume";
+    private const string MUSIC_KEY = "Music";
+    private const string SFX_KEY = "SFX";
+
+    [Serializable]
+    public struct Settings
+    {
+        public Setting setting;
+        public Slider slider;
+    }
+    public Settings[] settings;
+
+    private void Start()
+    {
+        // Initialize settings states; attempt to load from player prefs
+
+    }
+
+    // Save all settings based on settings dictionary to the respective play prefs entry
+    public void Save()
+    {
+        foreach (Settings s in settings)
+        {
+            Debug.Log("Setting: " + s.setting + ", Slider Value: " + s.slider.value);
+            PlayerPrefs.SetFloat(s.setting.ToString(), s.slider.value);
+        }
+    }
+}
