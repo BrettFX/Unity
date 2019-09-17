4851154d516f31ebed89ebcaebf574686f7c3493
Brett Allen <brettallen777@gmail.com>
2019-09-17 00:24:00 -0400

    Bind audio settings sliders to percentage sliders. Next steps are to get the audio controls functionality implemented

--- Babert Game/Assets/Scripts/ShowSliderValue.cs
+++ Babert Game/Assets/Scripts/ShowSliderValue.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class ShowSliderValue : MonoBehaviour
 {
+    public Text lblPercentage;
+
 	public void UpdateLabel (float value)
 	{
 		Text lbl = GetComponent<Text>();
 		if (lbl != null)
 			lbl.text = Mathf.RoundToInt (value * 100) + "%";
 	}
+
+    public void UpdateLabel(Slider slider)
+    {
+        lblPercentage.text = Mathf.RoundToInt(slider.value * 100) + "%";
+    }
 }
