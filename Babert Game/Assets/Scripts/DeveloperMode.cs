65c0b7979d630e2d91d3e9a8e1417ca98f82b848
Brett Allen <brettallen777@gmail.com>
2019-09-25 21:49:04 -0400

    Prepare to add developer mode feature to main menu User will have to click the game title in the menu an x number of times (TBD) to enable the developer mode Current plan is for developer mode to allow the user to manipulate the rocket speed, gravity, jump speed, etc.

---
+++ Babert Game/Assets/Scripts/DeveloperMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DeveloperMode : MonoBehaviour
+{
+    private int m_clicks = 0;
+
+    // Count number of clicks and enable developer mode
+    public void OnClick()
+    {
+        m_clicks++;
+        Debug.Log("Clicked " + m_clicks + " time(s)!");
+    }
+}
